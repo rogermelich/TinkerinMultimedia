9a1034e07b350c60b24d60fda7f63c7870f3ee10
Roger Melich <rogermelich@gmail.com>
2017-04-21 17:14:33 +0200

    Add Image poster

--- resources/views/videoJS.blade.php
+++ resources/views/videoJS.blade.php
@@ -11,8 +11,8 @@
             <div class="col-md-9 col-md-offset-1">
 
                 <video id="video1" class="video-js" controls
-                       preload="auto" width="640" height="264"
-                       data-setup="{}">
+                       preload="auto" width="640" height="460"
+                       poster="http://lorempixel.com/640/460" data-setup="{}">
                     <source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" type='video/mp4'>
                     <p class="vjs-no-js">El navegador no supporta aquests reproductor
                         <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
